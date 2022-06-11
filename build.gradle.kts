efd9d47a6e36f5141dc6ec01fe306dde5fb9555a
Martin Bonnin <martin@mbonnin.net>
2022-06-11 19:13:37 +0200

    use sjmp

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("org.jetbrains.kotlin.jvm").version("1.6.21")
-    id("maven-publish")
-    id("signing")
+    id("net.mbonnin.sjmp").version("0.2")
 }
 
 repositories {
@@ -13,119 +12,20 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
 }
 
-val emptyJavadocJarTaskProvider = tasks.register("emptyJavadocJar", org.gradle.jvm.tasks.Jar::class.java) {
-    archiveClassifier.set("javadoc")
+tasks.withType(JavaCompile::class.java) {
+    options.release.set(8)
 }
 
-val sourcesTaskProvider = tasks.register("javaSourcesJar", Jar::class.java) {
-    /**
-     * Add a dependency on the compileKotlin task to make sure the generated sources like
-     * antlr or SQLDelight get included
-     * See also https://youtrack.jetbrains.com/issue/KT-47936
-     */
-    dependsOn("compileKotlin")
-
-    archiveClassifier.set("sources")
-    val sourceSets = project.extensions.getByType(JavaPluginExtension::class.java).sourceSets
-    from(sourceSets.getByName("main").allSource)
-}
-
-publishing {
-    publications {
-        create("default", MavenPublication::class.java) {
-            from(components.findByName("java"))
-
-            artifact(emptyJavadocJarTaskProvider.get())
-            artifact(sourcesTaskProvider.get())
-
-            artifactId = "xoxo"
+sjmp {
+    jvmProject {
+        publication {
             groupId = "net.mbonnin.xoxo"
             version = "0.2-SNAPSHOT"
-
-            pom {
-                name.set(artifactId)
-                description.set(artifactId)
-
-                val githubUrl = "https://github.com/martinbonnin/xoxo"
-
-                url.set(githubUrl)
-
-                scm {
-                    url.set(githubUrl)
-                    connection.set(githubUrl)
-                    developerConnection.set(githubUrl)
-                }
-
-                licenses {
-                    license {
-                        name.set("MIT License")
-                        url.set("$githubUrl/blob/main/LICENSE")
-                    }
-                }
-
-                developers {
-                    developer {
-                        id.set("Xoxo authors")
-                        name.set("Xoxo authors")
-                    }
-                }
+            simplePom {
+                githubRepository = "martinbonnin/xoxo"
+                githubLicensePath = "LICENSE"
+                license = "MIT License"
             }
         }
-    }
-    repositories {
-        maven {
-            name = "ossSnapshots"
-            url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
-            credentials {
-                username = System.getenv("OSSRH_USER")
-                password = System.getenv("OSSRH_PASSWORD")
-            }
-        }
-        maven {
-            name = "ossStaging"
-            url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
-            credentials {
-                username = System.getenv("OSSRH_USER")
-                password = System.getenv("OSSRH_PASSWORD")
-            }
-        }
-    }
-}
-
-signing {
-    useInMemoryPgpKeys(System.getenv("GPG_PRIVATE_KEY"), System.getenv("GPG_PRIVATE_KEY_PASSWORD"))
-    sign(publishing.publications)
-}
-
-tasks.withType(Sign::class.java).configureEach {
-    isEnabled = !System.getenv("GPG_PRIVATE_KEY").isNullOrBlank()
-}
-
-tasks.register("ci") {
-    dependsOn("build")
-    if (shouldPublishSnapshots()) {
-        dependsOn(tasks.named("publishAllPublicationsToOssSnapshotsRepository"))
-    }
-    if (isTag()) {
-        dependsOn(tasks.named("publishAllPublicationsToOssStagingRepository"))
-    }
-}
-
-fun shouldPublishSnapshots(): Boolean {
-    val eventName = System.getenv("GITHUB_EVENT_NAME")
-    val ref = System.getenv("GITHUB_REF")
-
-    return eventName == "push" && (ref == "refs/heads/main")
-}
-
-fun isTag(): Boolean {
-    val ref = System.getenv("GITHUB_REF")
-
-    return ref?.startsWith("refs/tags/") == true
-}
-
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(8))
     }
 }
---
+++ settings.gradle.kts
@@ -0,0 +1,5 @@
+
+
+pluginManagement {
+//    includeBuild("../sjmp")
+}
